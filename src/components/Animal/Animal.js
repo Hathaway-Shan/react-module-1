e85b0c383f7bf43774ca538de5b0adcdf180c85e
Hathaway-Shan <hathaway.shan@gmail.com>
2022-09-12 15:22:37 -0700

    animal names and pictures are displaying

--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -1,8 +1,8 @@
 export default function Animal(props) {
   return (
-    <div className="animal">
-      Animal: {props.name} {props.says}
-      <img src={`${process.env.PUBLIC_URL}/${props.name}.webp`} />
+    <div className="animal" style={{ top: props.top, left: props.left }}>
+      Animal: {props.name}
+      <img src={`${process.env.PUBLIC_URL}/animals/${props.type}.svg`} />
     </div>
   );
 }
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -5,5 +5,11 @@ import { animals } from '../../data.js';
 import Animal from '../Animal/Animal.js';
 import background from '../../background.png';
 export default function Main() {
-  return <main style={{ backgroundImage: `url(${background})` }} className="App"></main>;
+  return (
+    <main style={{ backgroundImage: `url(${background})` }} className="App">
+      {animals.map((animal) => (
+        <Animal key={animal.name} {...animal} />
+      ))}
+    </main>
+  );
 }
