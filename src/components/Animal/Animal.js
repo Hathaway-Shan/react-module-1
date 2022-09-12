cfd0e33bb12ddbaa144249268db4a13971d24b51
Hathaway-Shan <hathaway.shan@gmail.com>
2022-09-12 15:38:24 -0700

    animals images names and says are displaying properly

--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -1,8 +1,10 @@
+import './Animal.css';
 export default function Animal(props) {
   return (
     <div className="animal" style={{ top: props.top, left: props.left }}>
-      Animal: {props.name}
       <img src={`${process.env.PUBLIC_URL}/animals/${props.type}.svg`} />
+      <span>{props.name}</span>
+      <span>{props.says}</span>
     </div>
   );
 }
